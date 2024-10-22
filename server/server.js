e2fcbf7390451c1c83539ac4a391bb9a784ba7b3
volto9 <volto9@yahoo.com>
2024-10-22 08:47:39 +0100

    first

---
+++ server/server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import cors from 'cors';
+
+// setup the server
+const app = express();
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Hello world!');
+});
+
+app.listen(8080, function () {
+  console.log(`Server is running on port 8080`);
+});
